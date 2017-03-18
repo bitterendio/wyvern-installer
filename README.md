6203e7927ddd8afc51274b231ac2d34a1c38f161
Matej Lukas <matejlukas12@gmail.com>
2017-03-18 21:37:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wyvern Installer
+Package for installation of [Wyvern theme](https://github.com/madesane/wyvern)
+
+Install globally
+```
+npm install -g wyvern-installer
+```
+
+Than use command
+```
+wyvern
+```
+
+With wyvern-installer you could install:
+* Wordpress + Wyvern + Wyvern Child
+* Wordpress + Wyvern + Wyvern Shop
+* Wordpress + Wyvern
+* Wyvern + Wyvern Child
+* Wyvern
---
+++ index.js
@@ -0,0 +1,186 @@
+#!/usr/bin/env node
+
+var inquirer = require('inquirer');
+var clone = require('git-clone');
+
+const repositories = {
+  'wyvern': 'https://github.com/madesane/wyvern.git',
+  'wyvern-child': 'https://github.com/madesane/wyvern-child.git',
+  'wyvern-shop': 'https://github.com/madesane/wyvern-shop.git',
+  'wordpress': 'https://github.com/WordPress/WordPress.git'
+};
+
+let listAnswer = {};
+let inputAnswer = {};
+
+/* List questions */
+const listQuestions = [
+  {
+    type: 'list',
+    name: 'installation',
+    message: 'What kind of installation do you want?',
+    choices: [
+      new inquirer.Separator('=== With Wordpress ==='),
+      {
+        name: 'Wordpress + Wyvern + Wyvern Child',
+        value: {
+          slug: ['wordpress', 'wyvern', 'wyvern-child'],
+          type: 'wordpress',
+          count: 3,
+        },
+      },
+      {
+        name: 'Wordpress + Wyvern + Wyvern Shop',
+        value: {
+          slug: ['wordpress', 'wyvern', 'wyvern-shop'],
+          type: 'wordpress',
+          count: 3,
+        },
+      },
+      {
+        name: 'Wordpress + Wyvern',
+        value: {
+          slug: ['wordpress', 'wyvern'],
+          type: 'wordpress',
+          count: 2,
+        },
+      },
+      new inquirer.Separator('=== Just theme(s) ==='),
+      {
+        name: 'Wyvern + Wyvern Child',
+        value: {
+          slug: ['wyvern', 'wyvern-child'],
+          type: 'theme',
+          count: 2,
+        },
+      },
+      {
+        name: 'Wyvern',
+        value: {
+          slug: ['wyvern'],
+          type: 'theme',
+          count: 1,
+        },
+      },
+      {
+        name: 'Wyvern Child',
+        value: {
+          slug: ['wyvern-child'],
+          type: 'theme',
+          count: 1,
+        },
+      },
+      {
+        name: 'Wyvern Shop',
+        value: {
+          slug: ['wyvern-shop'],
+          type: 'theme',
+          count: 1,
+        },
+      }
+    ],
+  }
+];
+
+inquirer.prompt(listQuestions).then((answer) => {
+  listAnswer = answer.installation;
+  const type = listAnswer.type;
+  const count = listAnswer.count;
+  const slug = listAnswer.slug;
+
+  function isType(installationType) {
+    return installationType === type;
+  };
+
+  const inputs = [
+    // Wordpress installations
+    {
+      type: 'input',
+      name: 'path[0]',
+      message: 'Where to clone Wordpress?',
+      default: function () {
+        return 'wordpress';
+      },
+      when: isType('wordpress'),
+    },
+    {
+      type: 'input',
+      name: 'path[1]',
+      message: 'Name of ' + slug[1] + ' theme?',
+      default() {
+        return slug[1];
+      },
+      when: isType('wordpress') && count > 1,
+    },
+    {
+      type: 'input',
+      name: 'path[2]',
+      message: 'Name of ' + slug[2] + ' theme?',
+      default() {
+        return slug[2];
+      },
+      when: isType('wordpress') && count > 2,
+    },
+    // Just themes installation
+    {
+      type: 'input',
+      name: 'path[0]',
+      message: 'Where to clone ' + slug[0] + ' theme?',
+      default() {
+        return slug[0];
+      },
+      when: isType('theme') && count >= 1,
+    },
+    {
+      type: 'input',
+      name: 'path[1]',
+      message: 'Where to clone ' + slug[1] + ' theme?',
+      default() {
+        return slug[1];
+      },
+      when: isType('theme') && count > 1,
+    },
+  ];
+
+  inquirer.prompt(inputs).then((inputsAnswer) => {
+    inputAnswer = inputsAnswer;
+    const paths = inputAnswer.path;
+
+    // Just theme
+    if (type === 'theme') {
+      for (let i = 0; i < count; i++) {
+        console.log('Cloning ' + slug[i] + ' theme from ' + repositories[slug[i]] + ' to /' + paths[i] + '...');
+        clone(repositories[slug[i]], paths[i], [], function (err) {
+          if (typeof err === 'undefined') {
+            console.log(slug[i] + ' DONE!');
+          } else {
+            console.log(err);
+          }
+        })
+      }
+    // With wordpress
+    } else if (type === 'wordpress') {
+      const themesPath = paths[0] + '/wp-content/themes/';
+      console.log('Cloning ' + slug[0] + ' from ' + repositories[slug[0]] + ' to /' + paths[0] + '...');
+      // Clone wordpress
+      clone(repositories[slug[0]], paths[0], [], function(err) {
+        if (typeof err === 'undefined') {
+          console.log(slug[0] + ' DONE!');
+          // Clone theme(s)
+          for (let i = 1; i < count; i++) {
+            console.log('Cloning ' + slug[i] + ' theme from ' + repositories[slug[i]] + ' to /' + themesPath + paths[i] + '...');
+            clone(repositories[slug[i]], themesPath + paths[i], [], function (err) {
+              if (typeof err === 'undefined') {
+                console.log(slug[i] + ' DONE!');
+              } else {
+                console.log(err);
+              }
+            })
+          }
+        } else {
+          console.log(err);
+        }
+      });
+    }
+  });
+});
